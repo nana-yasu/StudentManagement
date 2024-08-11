121e51a2a0854702a57a81ba8d77c5a1db430142
yasumotonana <ooinenn@yahoo.co.jp>
2024-08-11 14:55:29 +0900

    初回コミット

--- src/main/java/raise/tech/student/management/Application.java
+++ src/main/java/raise/tech/student/management/Application.java
@@ -13,7 +13,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 @GetMapping("/hello")
-	public String  hello() {
+	public String hello() {
 	 return "Hello,World!";
  }
 }
